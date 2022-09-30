c380d69039c507ffce5a7c7fe34537c884740a80
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-09-30 16:14:53 -0600

    Added findKeyByValue.js and completed it

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  let truthy = `✅ Assertion Passed: ${actual} === ${expected}`;
+  let falsy = `🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  return (actual === expected) ? truthy : falsy;
+};
+
+const findKeyByValue = (object, value) => {
+  for (let values in object) {
+    if (object[values] === value) {
+      return values;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
