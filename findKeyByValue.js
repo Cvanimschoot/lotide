f01acc1ee8b9868cd46616929fcd630b46145168
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-10-22 03:25:17 -0600

    Added findKeyByValueTest.js to test findKeyByValue() functionality using Mocha and Chai. Removed assertEqual from findKeyByValue.js as it is no longer needed. Removed console.log() from findKeyByValue.js as it is no longer needed with implementation of Mocha and Chai.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let truthy = `✅ Assertion Passed: ${actual} === ${expected}`;
-  let falsy = `🛑 Assertion Failed: ${actual} !== ${expected}`;
-
-  return (actual === expected) ? truthy : falsy;
-};
-
 const findKeyByValue = (object, value) => {
   for (let values in object) {
     if (object[values] === value) {
@@ -14,12 +7,3 @@ const findKeyByValue = (object, value) => {
 };
 
 module.exports = findKeyByValue;
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
