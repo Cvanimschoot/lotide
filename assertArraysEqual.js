0dccc8761d36b80042644acfb17b3c28abc42c14
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-10-22 02:29:44 -0600

    Changed assertArraysEqualTest.js to utilize Mocha and Chai. Created test cases for it. Also changed the functionality of assertArraysEqual. Before it would return a string telling the console true or false. Now it returns just true or false, to line up better for functionality.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,11 @@
 const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = (arrayOne, arrayTwo) => {
-  let truthy = `✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`;
-  let falsy = `🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`;
+  //let truthy = `✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`;
+  //let falsy = `🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`;
   let returnedBoolean = eqArrays(arrayOne, arrayTwo);
 
-  return (returnedBoolean) ? truthy : falsy;
+  return (returnedBoolean) ? true : false;
 };
 
 module.exports = assertArraysEqual;
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+let assertArraysEqual = require('../index')
+assertArraysEqual = assertArraysEqual.assertArraysEqual;
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 4]));
-console.log(assertArraysEqual([1, 2, 3], [1, 2, '3']));
+describe("#assertArraysEqual", () => {
+  it("returns true for [1,2,3] & [1,2,3]", () => {
+    assert.strictEqual(assertArraysEqual([1,2,3], [1,2,3]), true);
+  });
+  it("returns false for [1,2,3] & [1,2,4]", () => {
+    assert.strictEqual(assertArraysEqual([1,2,3], [1,2,4]), false);
+  });
+  it("returns false for [1,2,3] & [1,2,'3']", () => {
+    assert.strictEqual(assertArraysEqual([1,2,3], [1,2,'3']), false);
+  });
+});
